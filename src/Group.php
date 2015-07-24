c997212ec6d26a466146124d93649a513a45463d
Radu <radu.bordei89@gmail.com>
2015-07-24 14:29:27 +0300

    implements iterator.

--- src/Group.php
+++ src/Group.php
@@ -5,7 +5,7 @@ namespace Flysap\Support;
 use Flysap\Support\Traits\ElementAttributes;
 use Flysap\Support\Traits\ElementsTrait;
 
-class Group {
+class Group implements \Iterator {
 
     use ElementsTrait, ElementAttributes;
 
@@ -57,4 +57,65 @@ class Group {
         }
     }
 
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current() {
+        $element = current($this->elements);
+
+        return $element;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next() {
+        $element = next($this->elements);
+
+        return $element;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key() {
+        $element = key($this->elements);
+
+        return $element;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid() {
+        $key = key($this->elements);
+
+        $element = ($key !== NULL && $key !== FALSE);
+
+        return $element;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind() {
+        reset($this->elements);
+    }
 }
